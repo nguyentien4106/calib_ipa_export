2862c042356f959e54dceebb2db363df6fe06b6a
walter.nguyen <walter.nguyen@nexcel.com>
2024-12-26 10:03:02 +0700

    create ble

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ipa_testing_github_action/main.dart';
+import 'package:calib_ipa_export/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
